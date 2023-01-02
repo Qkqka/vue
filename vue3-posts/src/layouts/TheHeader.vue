323c0497a53aebd047cf63ea7d40e62499fa6eb4
fnfnksb <fnfnksb@gmail.com>
2023-01-02 18:01:53 +0900

    '230102'

--- vue3-posts/src/layouts/TheHeader.vue
+++ vue3-posts/src/layouts/TheHeader.vue
@@ -1,53 +1,73 @@
 <template>
-  <header>
-    <nav
-      class="navbar navbar-expand-sm navbar-dark bg-body-tertiary bg-primary"
-    >
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">GYM CODING</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+    <header>
+        <nav
+            class="navbar navbar-expand-sm navbar-dark bg-body-tertiary bg-primary"
         >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto">
-            <li class="nav-item">
-              <router-link class="nav-link" to="/">Home</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/about">About</router-link>
-            </li>
-            <!-- <li class="nav-item">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">GYM CODING</a>
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div
+                    class="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                >
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/">
+                                Home
+                            </router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/about">
+                                About
+                            </router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/posts">
+                                게시글
+                            </router-link>
+                        </li>
+                        <!-- <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="/">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="/about">About</a>
             </li> -->
-          </ul>
-          <form class="d-flex" role="search">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
-      </div>
-    </nav>
-  </header>
+                    </ul>
+                    <div class="d-flex">
+                        <button
+                            class="btn btn-outline-success"
+                            type="button"
+                            @click="goPage"
+                        >
+                            글쓰기
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const goPage = () => {
+    router.push({
+        name: 'PostCreate',
+    });
+    // ('/posts/create');
+};
+</script>
 
 <style lang="scss" scoped></style>
---
+++ vue3-posts/src/views/posts/PostEditView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h2>게시글 수정</h2>
+        <hr class="my-4" />
+        {{ $route.params }}
+    </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
